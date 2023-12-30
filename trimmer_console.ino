a69f4e089c5d946900dc564fbfde5a8005017190
Kohei Kajimoto <koheik@koheik.com>
2023-12-30 13:15:01 +1100

    adds two buttons

--- trimmer_console.ino
+++ trimmer_console.ino
@@ -12,8 +12,8 @@
 Joystick_ Joystick(
   JOYSTICK_DEFAULT_REPORT_ID,
   JOYSTICK_TYPE_JOYSTICK,
-  0, 0,                 // button, hat
-  true, true, false,    // x, y, z
+  2, 0,                 // button, hat
+  true, false, false,    // x, y, z
   false, false, false,  // rx, ry, rz
   false, false,         // rudder, throttler
   false, false, false   // accel, brake, steering
@@ -21,20 +21,33 @@ Joystick_ Joystick(
 
 unsigned long t_prev;
 int sensorPinX;
+int sensorPinB1;
+int sensorPinB2;
 int x_prev;
 double x_norm_prev;
 double x_gain;
+int b1_prev;
+int b2_prev;
 
 void setup() {
   Joystick.setXAxisRange(-127, 127);
   Joystick.begin(false);
+
   sensorPinX = A0;
+  sensorPinB1 = A2;
+  sensorPinB2 = A3;
+
   pinMode(sensorPinX, INPUT);
+  pinMode(sensorPinB1, INPUT);
+  pinMode(sensorPinB2, INPUT);
 
   t_prev = millis();
   x_prev = 0;
   x_norm_prev = 0.0;
   x_gain = 0.25;
+
+  b1_prev = 0;
+  b2_prev = 0;
 }
 
 void loop() {
@@ -55,8 +68,8 @@ void loop() {
   int sensorValueX = analogRead(sensorPinX);
   DEBUG_PRINT("sensorValueX=");
   DEBUG_PRINTLN(sensorValueX);
- 
-  if (abs(sensorValueX - 512) <= 1) {
+
+  if (abs(sensorValueX - 512) <= 3) {
     sensorValueX = 512;
   }
   double x_norm = (sensorValueX - 512) / 410.0;
@@ -76,6 +89,24 @@ void loop() {
   DEBUG_PRINTLN(x);
 
   Joystick.setXAxis(x);
+
+  int b1 = digitalRead(sensorPinB1);
+  int b2 = digitalRead(sensorPinB2);
+
+  DEBUG_PRINT("b1=");
+  DEBUG_PRINTLN(b1);
+
+  DEBUG_PRINT("b2=");
+  DEBUG_PRINTLN(b2);
+
+  if (b1 != b1_prev) {
+    Joystick.setButton(0, b1);
+  }
+
+  if (b2 != b2_prev) {
+    Joystick.setButton(1, b2);
+  }
+
   Joystick.sendState();
 
   t_prev = t;
